09dae59738d4a6bcd1e3f067e31d9ab32198a40d
akvergaray <akvergaray@baufest.com>
2021-08-14 00:00:39 -0500

    Cambiando nombre de API'S de clientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const router = Router();
 
 const { getClients, createClient, getAverage } = require ('../controllers/index.controller');
 
-router.get('/clients', getClients)
-router.post('/clients', createClient)
-router.get('/clients/promedio-edades', getAverage)
+router.get('/clientes', getClients)
+router.post('/clientes', createClient)
+router.get('/clientes/promedio-edades', getAverage)
 
 module.exports = router;
