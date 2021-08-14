32d521c0ee9e24aa2178f2182f33d697d48014ba
akvergaray <akvergaray@baufest.com>
2021-08-13 23:57:47 -0500

    Cambiando nombre de APIS

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const router = Router();
 
 const { getClients, createClient, getAverage } = require ('../controllers/index.controller');
 
-router.get('/users', getClients)
-router.post('/users', createClient)
-router.get('/users/promedio-edades', getAverage)
+router.get('/clients', getClients)
+router.post('/clients', createClient)
+router.get('/clients/promedio-edades', getAverage)
 
 module.exports = router;
