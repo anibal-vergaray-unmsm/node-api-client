ab90a43c0416a4566f142ff822a1af01656db4e4
akvergaray <akvergaray@baufest.com>
2021-11-04 23:56:39 -0500

    Modificando connectionString

---
+++ src/developmentSettings.js
@@ -0,0 +1,3 @@
+module.exports = Object.freeze({
+    connectionString: 'postgres://sooqmrmbakkjfq:fcc3190aacf918b10cd056e75a6c97f323894a5b2c1ce050229ded7870e98e93@ec2-3-217-68-126.compute-1.amazonaws.com:5432/dec5ovvd6q2sj6',
+});
